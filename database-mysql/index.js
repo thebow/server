b59bcafa60f055172fc4a62a6cbc79a2e97a7988
SlimGuenidez <leagueofslim@outlook.com>
2022-04-22 13:16:45 +0200

    edit

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -4,7 +4,7 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'passe1999',
+  password : 'Hackmeslim1998',
   database : 'eventogo'
 });
 
